c0d4dfd9919d414e4a265262633a68e9848c7859
kamalesshh <116070281+kamalesshh@users.noreply.github.com>
2023-06-15 12:03:25 +0530

    Delete custom directory

--- custom/myshoestore/myshoestorefacades/src/de/hybris/myshoestore/facades/loyalty/LoyaltyPointsFacades.java
+++
@@ -1,8 +0,0 @@
-package de.hybris.myshoestore.facades.loyalty;
-
-import de.hybris.myshoestore.facades.LoyaltyData;
-
-public interface LoyaltyPointsFacades
-{
-	public LoyaltyData defaultLoyaltyPointsFacade();
-}
